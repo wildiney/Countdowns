39b867739c6ba5d24328108c746167aa950d1621
Wildiney Di Masi <wildiney@gmail.com>
2024-01-21 12:20:09 -0300

    refactoring

--- app/components/layout/header.tsx
+++ app/components/layout/header.tsx
@@ -1,19 +1,21 @@
 "use client"
 import React, { useRef, useState } from 'react'
 import Logo from './logo'
+import Link from 'next/link'
 
-function Header ({ isHome, addCountdown }: { isHome?: boolean, addCountdown: (event: React.MouseEvent<HTMLDivElement | HTMLButtonElement>) => void }) {
+function Header ({ isHome, addCountdown }: { isHome?: boolean, addCountdown?: (event: React.MouseEvent<HTMLDivElement | HTMLButtonElement>) => void }) {
 
   return (
     <>
-      <div className='fixed top-0 left-0 flex items-center justify-between w-full p-6' >
-        <span className='hidden sm:block sm:w-64 '>
-          <Logo />
+      <div className='flex items-center justify-center w-full px-6 py-4 sm:justify-between' >
+        <span className={`${isHome ? "hidden sm:block" : null} w-52 sm:block sm:w-52 `}>
+          <Link href={'/'} ><Logo /></Link>
         </span>
-        <span>
-          <button
+        <span className={`${isHome ? "hidden sm:block" : null}`}>
+          {addCountdown && <button
             className='hidden px-3 py-2 text-white rounded-md bg-sky-900 hover:bg-sky-800 sm:flex'
             onClick={addCountdown}>+countdown</button>
+          }
         </span>
       </div>
     </>
