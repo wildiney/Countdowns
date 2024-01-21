c4e2a2969a123707d07c062893febf49ec912b75
Wildiney Di Masi <wildiney@gmail.com>
2024-01-21 12:22:48 -0300

    refactoring

--- app/page.tsx
+++ app/page.tsx
@@ -6,23 +6,35 @@ import Dialog from './components/layout/dialog'
 import { useState, useRef, useEffect } from 'react'
 import Link from 'next/link'
 
+type CountdownItem = {
+  name: string;
+  link: string;
+}
+
 export default function Home () {
   const [isOpen, setIsOpen] = useState(false)
   const eventName = useRef<HTMLInputElement>(null)
   const date = useRef<HTMLInputElement>(null)
   const [hasURL, setHasURL] = useState(false)
   const [url, setURL] = useState<string>('')
-  const [countdowns, setCountdowns] = useState<{ link: string }[]>([])
+  const [countdowns, setCountdowns] = useState<{ name?: String, link: string }[]>([])
   const [dateHasError, setDateHasError] = useState(false)
   const [eventNameHasError, seteventNameHasError] = useState(false)
   const [eventError, setEventError] = useState('')
   const [dateError, setDateError] = useState('')
 
   useEffect(() => {
-    const getLocalstorage: { link: string }[] = JSON.parse(localStorage.getItem("countdowns") || '[{link:""}]') || [{}]
-    const removeDuplicates: Set<{ link: string }> = new Set(getLocalstorage)
-    const array = [...removeDuplicates]
-    setCountdowns(array)
+    const localStorageData = localStorage.getItem("countdowns")
+    if (localStorageData) {
+      const parsedData: CountdownItem[] = JSON.parse(localStorageData)
+      const nonEmptyItems = parsedData.filter(item => item.link?.trim())
+      if (nonEmptyItems.length > 0) {
+        const removeDuplicates = Array.from(new Set(nonEmptyItems));
+        const data = removeDuplicates
+        setCountdowns(data)
+      }
+    }
+
   }, [])
 
   const modalHandler = (event: React.MouseEvent<HTMLDivElement | HTMLButtonElement>) => {
@@ -34,6 +46,15 @@ export default function Home () {
       setDateHasError(false)
       seteventNameHasError(false)
     }
+  }
+
+  const removeItem = (link: string) => {
+    const confirm = window.confirm("Are you sure?")
+    if (!confirm) return
+    const filtered = countdowns.filter((item) => {
+      return item.link != link
+    })
+    setCountdowns(filtered)
   }
 
   const submitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -65,13 +86,13 @@ export default function Home () {
       }
       const originalEventName = encodeURI(eventName.current?.value)
 
-      const newURL = `http://www.localhost:3000/${originalDate}/${originalEventName}`
+      const newURL = `${process.env.NEXT_PUBLIC_SITE_URL}/${originalDate}/${originalEventName}`
       setURL(newURL)
       setHasURL(true)
 
       if (!countdowns.some(item => item.link === newURL)) {
-        localStorage.setItem("countdowns", JSON.stringify([...countdowns, { link: newURL }]));
-        setCountdowns([...countdowns, { link: newURL }]);
+        localStorage.setItem("countdowns", JSON.stringify([...countdowns, { name: eventName.current.value, link: newURL }]));
+        setCountdowns([...countdowns, { name: eventName.current.value, link: newURL }]);
       }
     }
   }
@@ -79,10 +100,10 @@ export default function Home () {
   return (
     <>
       <Background>
-        <Header isHome={false} addCountdown={modalHandler} />
+        <Header addCountdown={modalHandler} isHome={true} />
 
-        <div className='flex items-center w-full h-full sm:hidden'>
-          <div className='flex flex-col max-w-sm gap-12 p-4 mx-auto -translate-y-16'>
+        <div className='flex items-center flex-1 w-full h-full '>
+          <div className='flex flex-col max-w-sm gap-12 p-4 mx-auto sm:hidden'>
             <Logo />
             <button
               className='flex justify-center px-3 py-2 text-white rounded-md sm:hidden bg-sky-900 hover:bg-sky-800 '
@@ -90,15 +111,28 @@ export default function Home () {
           </div>
         </div>
 
-        {countdowns.length > 1 &&
-          <div className='fixed bottom-0 flex flex-col flex-1 w-full mb-6'>
+        {countdowns.length > 0 &&
+          <div className='flex flex-col mb-6 full '>
             <h3 className='px-6 mb-2 text-white drop-shadow-[2px_2px_1px_rgba(0,0,0,0.5)]'>COUNTDOWNS</h3>
-            <ul className='flex flex-wrap gap-4 mx-6'>
+            <ul className='flex flex-wrap items-center justify-center gap-4 mx-6 overflow-y-auto max-h-[40vh] min-h-min pr-4'>
               {countdowns.map((countdown, index) => (
-                <li className='p-4 text-center bg-white rounded-md first:hidden bg-opacity-80 indent-0 grow' key={index}>
-                  {typeof (countdown.link) == 'string' ? <Link className='text-sm break-all' href={countdown.link}>{countdown.link}</Link> : null}
+
+
+                <li className='flex items-center justify-between p-4 text-center bg-white rounded-md bg-opacity-80 indent-0 grow' key={index}>
+                  {typeof (countdown.link) == 'string' ? <div className='flex items-center justify-between w-full gap-6'>
+                    <Link className='flex flex-col w-full text-sm text-left break-all' href={countdown.link}>
+                      <div className='flex justify-start text-lg font-bold uppercase text-sky-950'>{countdown.name}</div>
+                      <div className='flex justify-start'>{countdown.link}</div>
+                    </Link>
+                    <button onClick={() => removeItem(countdown.link)}>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className='fill-sky-950 hover:fill-red-900'>
+                        <path d="M19 4h-3.5l-1-1h-5l-1 1H5v2h14M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6z" /></svg>
+                    </button>
+                  </div> : null}
                 </li>
-              ))}
+
+              ))
+              }
             </ul>
           </div>}
 
